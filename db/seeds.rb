d430c9ce142e270b159fd6dbbaa3b5afbe78d663
Junya Wako <j.wako@j-q.co.jp>
2015-03-03 09:27:09 +0900

    rails_apps_composer: set up database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = CreateAdminService.new.call
+puts 'CREATED ADMIN USER: ' << user.email
+# Environment variables (ENV['...']) can be set in the file .env file.
